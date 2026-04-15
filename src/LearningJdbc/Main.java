a6f0d3e18ad83faabaf45c68823dca7b3d8ef097
Pranav <boyp56039@gmail.com>
2026-04-15 21:51:29 +0530

    Basic crud in jdbc

--- src/LearningJdbc/Main.java
+++ src/LearningJdbc/Main.java
@@ -22,18 +22,61 @@ public class Main{
 
     //execute query and prcoess result
 
-    String sql= "INSERT INTO studentinfo (id,sname,sage,scity) VALUES (2,'Pranav',23,'Hoshiarpur')";
+//    String sql= "INSERT INTO studentinfo (id,sname,sage,scity) VALUES (2,'Pranav',23,'Hoshiarpur')";
+//
+//    int rowsaffected=statement.executeUpdate(sql);
+//
+//    if(rowsaffected==0) System.out.println("Unable to insert");
+//    else{
+//      System.out.println("Sucess");
+//    }
 
-    int rowsaffected=statement.executeUpdate(sql);
+//    String sql2= "Update studentinfo set sage=26 where id=2";
+//    int rowsaffected2=statement.executeUpdate(sql2);
+//      if(rowsaffected2==0) System.out.println("Unable to update");
+//      else{
+//          System.out.println("Sucess update");
+//      }
 
-    if(rowsaffected==0) System.out.println("Unable to insert");
-    else{
-      System.out.println("Sucess");
-    }
+
+//      String sql3="Select * from studentinfo";
+//      ResultSet rs= statement.executeQuery(sql3);
+//
+//      while(rs.next()){
+//          System.out.println(rs.getInt(1)+" "+ rs.getString(2));
+//      }
+
+//      String sql4="Delete from studentinfo where id=2";
+//      int rowaffected= statement.executeUpdate(sql4);
+//
+//      if(rowaffected==0) System.out.println("Nothing happeed");
+//      else System.out.println("Success delete");
+
+      String sql= "update studentinfo set sage=29 where id=1";
+      boolean status= statement.execute(sql); //wii return if non update statment
+
+      if(status){
+
+          ResultSet rs= statement.getResultSet();
+
+          while(rs.next()){
+              System.out.println(rs.getInt(1)+" "+ rs.getString(2));
+          }
+          rs.close();
+      }else{
+
+          int rowsaffected= statement.getUpdateCount();
+          if(rowsaffected==0) System.out.println("Nothing happened");
+          else{
+              System.out.println("Sucess update");
+          }
+
+      }
 
 
 
     //closing resources
+//      rs.close();
     statement.close();
     connect.close();
 
